ae056feaf404d4926893493441e78e46296508fa
Hassan <zaaferani.arta@gmail.com>
2021-12-27 17:30:23 +0330

    fix bug on header with file response

--- src/Middleware/CacheWithVarnish.php
+++ src/Middleware/CacheWithVarnish.php
@@ -8,11 +8,21 @@ class CacheWithVarnish
 {
     public function handle($request, Closure $next, int $cacheTimeInMinutes = null)
     {
-        $response = $next($request);
+        try{
+            $response = $next($request);
+			if (method_exists($response,'withHeaders')){
+                return $response->withHeaders([
+                    config('varnish.cacheable_header_name') => '1',
+                    'Cache-Control' => 'public, max-age='. 60 * ($cacheTimeInMinutes ?? config('varnish.cache_time_in_minutes')),
+                ]);
+			} else {
+				$request->headers->set( config('varnish.cacheable_header_name'), '1');
+				$request->headers->set( 'Cache-Control', 'public, max-age='. 60 * ($cacheTimeInMinutes ?? config('varnish.cache_time_in_minutes')));
 
-        return $response->withHeaders([
-            config('varnish.cacheable_header_name') => '1',
-            'Cache-Control' => 'public, max-age='. 60 * ($cacheTimeInMinutes ?? config('varnish.cache_time_in_minutes')),
-        ]);
+			    return $next($request);
+			}
+	    } catch (\Exception $e){
+		    return $next($request);
+	    }        
     }
 }
